450d9ef2e77b731038f2f3bd70adcd5cd03ac641
asahi <asahi5286@icloud.com>
2023-05-16 15:09:15 +0900

    第2章練習問題を追加

---
+++ ControlCheck/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            labelCheckBox.Text
+                = "チェックボックス:" + checkBox1.Checked;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+           labelCheckBox.Text
+                ="チェックボックス :" + checkBox1.Checked;
+            labelRadioButton1.Text
+                ="ラジオボタン１ :" + radioButton1.Checked;
+            labelRadioButton2.Text
+                ="ラジオボタン２ :" + radioButton2.Checked;
+            labelNumericUpDown.Text
+                ="数値 :" + numericUpDown1.Value;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            labelRadioButton1.Text
+                = "ラジオボタン１ :" + radioButton1.Checked;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            labelRadioButton2.Text
+                = "ラジオボタン２ :" + radioButton2.Checked;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            labelNumericUpDown.Text
+                = "数値 :" + numericUpDown1.Value;
+        }
+    }
+}
