1e383810eafc011d97f5c821f9b2a1b27c4afb44
keitakano <takanoike2050@gmail.com>
2023-05-16 15:12:59 +0900

    第2章練習問題を追加

---
+++ 練習問題/練習問題/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 練習問題
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelCheckBox.Text = "チェックボックス：" + checkBox1.Checked;
+            labelRadioButton1.Text = "ラジオボタン１ :" + radioButton1.Checked;
+            labelCheckButton2.Text = "ラジオボタン２ :" + radioButton2.Checked;
+            labelNumericUpDown.Text = "数値：" + numericUpDown1.Value;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            labelCheckBox.Text = "チェックボックス：" + checkBox1.Checked;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            labelRadioButton1.Text = "ラジオボタン１ :" + radioButton1.Checked;
+
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            labelCheckButton2.Text = "ラジオボタン２ :" + radioButton2.Checked;
+
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            labelNumericUpDown.Text = "ニューメリックアップダウン：" + numericUpDown1.Value;
+
+
+
+        }
+
+        private void labelCheckBox_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void labelNumericUpDown_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
