8218390320b3957ae59d31a651099ba9f2846397
前田　丈 <133735669+maaeda@users.noreply.github.com>
2023-05-17 14:22:09 +0900

    add studyp071

---
+++ StudyP071/StudyP071/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyP071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //変数の宣言
+            int a;
+            double x, y;
+            
+            //変数の代入
+            a = 123;
+
+            //変数参照
+            Console.WriteLine(a);
+        }
+    }
+}
