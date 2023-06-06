dd36fa6e765d8a027f0ab5dd0ca2ae87084ef073
MSI\0cc42 <0cc42x022p.2v6j@au.com>
2023-06-06 15:13:35 +0900

    第3章練習問題追加

---
+++ SplitCost/SplitCost/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplitCost
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label6.Text = "";
+            label8.Text = "";
+
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int money;
+            int people;
+            double addTax;
+            int price;
+            int mod;
+            const double Tax = 0.1;
+
+            money = int.Parse(textBoxMoney.Text);
+            people = int.Parse(People.Text);
+
+            addTax = money;
+            addTax *= (1 + Tax);
+
+            price = (int)addTax / people;
+            mod = (int)addTax % people;
+
+            label6.Text = price + "円";
+            label8.Text = mod + "人";
+        }
+    }
+}
