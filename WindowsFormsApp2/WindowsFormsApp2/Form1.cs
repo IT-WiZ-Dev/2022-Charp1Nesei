6cf0d20356d8b9603fa50dbca7a98c39afe6a676
MSI\81906 <hm.srkyn@au.com>
2023-06-06 15:10:32 +0900

    第３章練習問題を追加

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label7.Text = "";
+            label8.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int money;
+            int people;
+            double addtax;
+            int hitori;
+            int mod;
+            const double Tax = 0.1;
+
+            money = int.Parse(textBox2.Text);
+            people = int.Parse(textBox1.Text);
+
+            addtax = money;
+            addtax *= (1 + Tax);
+            hitori = (int)addtax / people;
+            mod = (int)addtax % people;
+
+            label7.Text = hitori +"円" ;
+            label8.Text = mod + "円";
+
+
+
+
+
+
+
+
+        }
+    }
+}
