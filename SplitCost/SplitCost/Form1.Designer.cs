a2129f1aafe8bed714a829d65fc839df66604ad7
太田　陸翔 <inbederacade@gmail.com>
2023-06-06 15:10:27 +0900

    第3章練習問題を追加

---
+++ SplitCost/SplitCost/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace SplitCost
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxmoney = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxwarikan = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labeladdtax = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelamari = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "税抜き金額";
+            // 
+            // textBoxmoney
+            // 
+            this.textBoxmoney.Location = new System.Drawing.Point(15, 27);
+            this.textBoxmoney.Name = "textBoxmoney";
+            this.textBoxmoney.Size = new System.Drawing.Size(141, 22);
+            this.textBoxmoney.TabIndex = 1;
+            this.textBoxmoney.Text = "0";
+            this.textBoxmoney.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "円";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "人数";
+            // 
+            // textBoxwarikan
+            // 
+            this.textBoxwarikan.Location = new System.Drawing.Point(228, 27);
+            this.textBoxwarikan.Name = "textBoxwarikan";
+            this.textBoxwarikan.Size = new System.Drawing.Size(141, 22);
+            this.textBoxwarikan.TabIndex = 2;
+            this.textBoxwarikan.Text = "1";
+            this.textBoxwarikan.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(375, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "人";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "一人あたり";
+            // 
+            // labeladdtax
+            // 
+            this.labeladdtax.AutoSize = true;
+            this.labeladdtax.Location = new System.Drawing.Point(12, 115);
+            this.labeladdtax.Name = "labeladdtax";
+            this.labeladdtax.Size = new System.Drawing.Size(35, 15);
+            this.labeladdtax.TabIndex = 0;
+            this.labeladdtax.Text = "label";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(225, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 15);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "余り";
+            // 
+            // labelamari
+            // 
+            this.labelamari.AutoSize = true;
+            this.labelamari.Location = new System.Drawing.Point(225, 115);
+            this.labelamari.Name = "labelamari";
+            this.labelamari.Size = new System.Drawing.Size(35, 15);
+            this.labelamari.TabIndex = 0;
+            this.labelamari.Text = "label";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(382, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "計算する";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 226);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelamari);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.labeladdtax);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxwarikan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxmoney);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "割り勘";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxmoney;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxwarikan;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labeladdtax;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelamari;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SplitCost/SplitCost/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplitCost
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int money;
+            double addtax;
+            const double tax = 0.1;
+            int 人数;
+            
+
+            money = int.Parse(textBoxmoney.Text);
+            人数 = int.Parse(textBoxwarikan.Text);
+            addtax = money;
+            addtax *= (1 + tax);
+            money = (int)addtax;
+
+            labeladdtax.Text = (money/人数) + " 円";
+            labelamari.Text = (money % 人数) + " 円";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labeladdtax.Text = "";
+            labelamari.Text = "";
+        }
+    }
+}
