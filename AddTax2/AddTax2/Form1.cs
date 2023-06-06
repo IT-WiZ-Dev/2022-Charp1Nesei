284ccbf563dfa141d7a0e3bdb8ac3bf462e97f5a
AzureAD\笹原加慶(zeal23310180) <sasagreeeen@icloud.com>
2023-06-06 15:10:21 +0900

    第3章練習問題を追加

---
+++ AddTax2/AddTax2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddTax2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int money;
+            double addtax;
+            const double Tax = 0.1;
+            int number;
+            int waru;
+            int amari;
+
+            number = int.Parse(textBoxnumber.Text);
+
+            money = int.Parse(textBoxmoney.Text);
+
+            addtax = money;
+            addtax *= (1 + Tax);
+            money = (int)addtax;
+
+           
+            waru = money / number;
+            labelwaru.Text = waru + "円";
+
+            amari = money % number;
+            labelamari.Text = amari + "円";
+
+
+
+        }
+    }
+}
