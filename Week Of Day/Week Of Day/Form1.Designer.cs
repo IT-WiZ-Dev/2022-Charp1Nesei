cfcb82f9a37fd92172ff2571b4a866e217ca8f4b
MSI\igada <daiki20050226@gmail.com>
2023-07-13 21:26:34 +0900

    第4章練習問題を追加

---
+++ Week Of Day/Week Of Day/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Week_Of_Day
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDownMonth = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownDay = new System.Windows.Forms.NumericUpDown();
+            this.label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDownMonth
+            // 
+            this.numericUpDownMonth.Location = new System.Drawing.Point(322, 94);
+            this.numericUpDownMonth.Name = "numericUpDownMonth";
+            this.numericUpDownMonth.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDownMonth.TabIndex = 2;
+            // 
+            // numericUpDownDay
+            // 
+            this.numericUpDownDay.Location = new System.Drawing.Point(492, 94);
+            this.numericUpDownDay.Name = "numericUpDownDay";
+            this.numericUpDownDay.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDownDay.TabIndex = 3;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(655, 94);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(17, 18);
+            this.label.TabIndex = 4;
+            this.label.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.numericUpDownDay);
+            this.Controls.Add(this.numericUpDownMonth);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDownMonth;
+        private System.Windows.Forms.NumericUpDown numericUpDownDay;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ Week Of Day/Week Of Day/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_Of_Day
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool dayCehck = MonthDayCheck();
+            string week = WhatWeek();
+
+            if (dayCehck)
+            {
+                label.Text = week;
+            }
+            else
+            {
+                label.Text = "ありえない日付";
+            }
+
+        }
+
+        //うるう年かどうか判定するぞ！！！メソッド
+        private bool UruuYear()
+        {
+            {
+            int year = int.Parse(textBox1.Text);
+
+            bool isUruu = false;
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+               {
+                   isUruu = true;
+               }
+                
+                return isUruu;
+            }
+        }
+
+        //正しい月日が入力されたかどうか判断す
+        private bool MonthDayCheck()
+        {
+            bool isUruu = UruuYear();
+            bool dayCheck = true;
+            int year = int.Parse(textBox1.Text);
+            int month = numericUpDownMonth.DecimalPlaces;
+            int day = numericUpDownDay.DecimalPlaces;
+
+            //日付チェック
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+
+                    if (day > 31)
+                    {
+                        dayCheck = false;
+                    }
+                    break;
+            }
+
+            switch (month)
+            {
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    if (day > 30)
+                    {
+                        dayCheck = false;
+                    }
+                    break;
+            }
+            if (isUruu)
+            {
+                switch (month)
+                {
+                    case 2:
+                        if (day > 29)
+                        {
+                            dayCheck = false;
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                switch (month)
+                {
+                    case 2:
+                        if (day > 28)
+                        {
+                            dayCheck = false;
+                        }
+                        break;
+                }
+            }
+            return dayCheck;
+        }
+
+        //曜日を求める
+        private string WhatWeek()
+        {
+            string week="";
+            int year = int.Parse(textBox1.Text);
+            int day = (int)numericUpDownDay.Value;
+            int month = (int)numericUpDownMonth.Value;
+            int suu;
+
+            switch (month)
+            {
+                case 1:
+                case 2:
+                    month += 12;
+                    year -= 1;
+                break;
+            }
+
+            suu = (5 * year / 4 - year / 100 + year / 400 + (26 * month + 16) / 10 + day) % 7;
+
+            switch (suu)
+            {
+                case 0:
+                    week = "日曜日";
+                    break;
+                case 1:
+                    week = "月曜日";
+                    break;
+                case 2:
+                    week = "火曜日";
+                    break;
+                case 3:
+                    week = "水曜日";
+                    break;
+                case 4:
+                    week = "木曜日";
+                    break;
+                case 5:
+                    week = "金曜日";
+                    break;
+                case 6:
+                    week = "土曜日";
+                    break;
+            }
+            return week;
+        }
+    }
+}
