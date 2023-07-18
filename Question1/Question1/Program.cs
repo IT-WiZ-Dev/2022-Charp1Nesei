96d51645c13f82568b35aa90f8b952c17b62ca01
AzureAD\吉田温翔(zeal23310023) <zeai23310023@fsgci.jp>
2023-07-18 15:56:11 +0900

    前期課題

---
+++ Question1/Question1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問１
+ * 
+ * 次のMain関数内で宣言されている変数のデータ型を適切なものに修正して下さい。 
+ *
+ * 
+*/
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value1 = "Name";
+            {
+                value1 = (''); 
+            }
+            bool value2 = true;
+            {
+                value2 = ('');
+            }
+            char value3 = 'C';
+            {
+                value3 = ('');
+            }
+            double value4 = 10.5;
+            {
+                value4 = ('');
+            }
+            int value5 = 10;
+            {
+                value5 = ('');
+            }
+        }
+    }
+}
