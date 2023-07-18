5694584b19ce07e6ace8fd7aaed58a91eab54884
AzureAD\笹原加慶(zeal23310180) <sasagreeeen@icloud.com>
2023-07-18 16:13:41 +0900

    前期期末試験

---
+++ AddTax2/Question1/Question5/Question5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問5
+ * メソッドAddNumを定義して、メソッドMain内で呼び出しなさい。
+ * メソッド定義は下記の通りにすること
+ * 
+ * メソッド名：AddNum
+ * 戻り値の型：整数型
+ * 引数列　　：整数型 num1、整数型 num2
+ * 処理　　　：引数で渡されてきた2つの値を足し算した結果を戻り値として返す。
+ * 
+*/
+
+namespace Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int plus;
+            int num1;
+            int num2;
+            AddNum(10,20);
+            
+
+        }
+        private int AddNum(int num1,int num2);
+
+       
+            return plus;
+
+    }
+}
