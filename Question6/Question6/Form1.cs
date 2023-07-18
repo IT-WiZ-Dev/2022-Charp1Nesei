53bdc679ce8e155b6041016366d956203d38b454
seinomasa <hanwaku0721@gmail.com>
2023-07-18 16:13:33 +0900

    期末テスト

---
+++ Question6/Question6/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static void Main (string args)
+        {
+            Console.WriteLine();
+            Console.int.Parse(ReadLine)());
+            var b10000 = money 
+        }
+
+
+
+    }
+}
