4e6aec6bbe263e8d0bcc28205c0ad8ea9c6ac288
MSI\michi <mi.chi.ka10@icloud.com>
2023-07-18 15:53:10 +0900

    前期期末試験の解答を提出します

---
+++ SplitCost/Question4/Question4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問4
+ * 配列arrを定義し、配列内の値を全てfor文のループの中で出力（表示）しなさい。
+ * 配列arrは整数型の配列、要素数は『5』、添え字0～4にはそれぞれ
+ * 『10』『20』『30』『40』『50』
+ * の値を格納しなさい。
+*/
+
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[5];
+            arr[0] = 10;
+            arr[1] = 20;
+            arr[2] = 30;
+            arr[3] = 40;
+            arr[4] = 50;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ SplitCost/Question6/Question6/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxChange_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+
+            int oturi = int.Parse(textBoxChange.Text);
+
+            int gosen = oturi / 5000;
+            oturi = oturi % 5000;
+            int senen = oturi/1000;
+            oturi = oturi % 1000;
+            int gohyakuenn = oturi / 500;
+            oturi = oturi % 500;
+            int hyakuenn = oturi / 100;
+            oturi = oturi % 100;
+            int gozyuuenn = oturi / 50;
+            oturi = oturi% 50;
+            int zyuuenn = oturi/ 10;
+            oturi= oturi % 10;
+            int goenn = oturi / 5;
+            int itienn = oturi % 5;
+            
+
+            label5000yen.Text = gosen.ToString();
+            label1000yen.Text = senen.ToString();
+            label500yen.Text = gohyakuenn.ToString();
+            label100yen.Text = hyakuenn.ToString();
+            label50yen.Text = gozyuuenn.ToString();
+            label10yen.Text = zyuuenn.ToString();
+            label5yen.Text = goenn.ToString();
+            label1yen.Text = itienn.ToString();
+
+
+
+
+
+        }
+    }
+}
