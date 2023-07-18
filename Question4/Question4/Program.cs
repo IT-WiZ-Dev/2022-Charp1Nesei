b83cc841fc61db987581e7ec4c2794ff91b2a569
宮下　吟侍 <adgjmptw565@ickoud.com>
2023-07-18 16:22:01 +0900

    期末課題提出

---
+++ Question4/Question4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問4
+ * 配列arrを定義し、配列内の値を全てfor文のループの中で出力（表示）しなさい。
+ * 配列arrは整数型の配列、要素数は『5』、添え字0～4にはそれぞれ
+ * 『10』『20』『30』『40』『50』
+ * の値を格納しなさい。
+*/
+
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arr = new string[] { "10", "20", "30","40","50" };
+
+            for (var i = 0;  i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+
+
+        }
+    }
+}
