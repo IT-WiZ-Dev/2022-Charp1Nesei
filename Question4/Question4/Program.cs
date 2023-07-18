d47cae5d2b78b53951193c9f5776d960c6a37837
AzureAD\國島聡一郎(zeal23310176) <kunishima0912@gmail.com>
2023-07-18 16:06:12 +0900

    kimatu

---
+++ Question4/Question4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問4
+ * 配列arrを定義し、配列内の値を全てfor文のループの中で出力（表示）しなさい。
+ * 配列arrは整数型の配列、要素数は『5』、添え字0～4にはそれぞれ
+ * 『10』『20』『30』『40』『50』
+ * の値を格納しなさい。
+*/
+
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = new int[5] {10, 20, 30, 40, 50 };
+
+            for (var i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+
+        }
+    }
+}
---
+++ Question5/Question5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問5
+ * メソッドAddNumを定義して、メソッドMain内で呼び出しなさい。
+ * メソッド定義は下記の通りにすること
+ * 
+ * メソッド名：AddNum
+ * 戻り値の型：整数型
+ * 引数列　　：整数型 num1、整数型 num2
+ * 処理　　　：引数で渡されてきた2つの値を足し算した結果を戻り値として返す。
+ * 
+*/
+
+namespace Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calc.Addnum(3, 4));
+        }
+
+        class Calc
+        {
+            public static int Addnum(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
+    }
+}
