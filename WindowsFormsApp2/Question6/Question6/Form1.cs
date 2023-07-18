2742d7f8d6b4353c563124b3870dfcc083e1b002
MSI\81906 <hm.srkyn@au.com>
2023-07-18 16:13:48 +0900

    前期期末試験の解答の提出

---
+++ WindowsFormsApp2/Question6/Question6/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+            int yen, label5000, label1000, label500, label100, label50, label10, label5, label1;
+
+
+
+            
+
+
+
+
+
+
+        }
+    }
+}
