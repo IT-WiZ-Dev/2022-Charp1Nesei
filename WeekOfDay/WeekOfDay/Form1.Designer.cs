ab78831a185b369c868845e9cd74ad5fb99b6830
太田　陸翔 <inbederacade@gmail.com>
2023-07-18 13:15:43 +0900

    2～4章章末問題追加

---
+++ WeekOfDay/WeekOfDay/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace WeekOfDay
+{
+    partial class FormYoubi
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSeireki = new System.Windows.Forms.Label();
+            this.textBoxToshi = new System.Windows.Forms.TextBox();
+            this.labelTsuki = new System.Windows.Forms.Label();
+            this.numericTsuki = new System.Windows.Forms.NumericUpDown();
+            this.labelHi = new System.Windows.Forms.Label();
+            this.numericHi = new System.Windows.Forms.NumericUpDown();
+            this.labelYoubi = new System.Windows.Forms.Label();
+            this.buttonSanshutsu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericTsuki)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericHi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelSeireki
+            // 
+            this.labelSeireki.AutoSize = true;
+            this.labelSeireki.Location = new System.Drawing.Point(12, 9);
+            this.labelSeireki.Name = "labelSeireki";
+            this.labelSeireki.Size = new System.Drawing.Size(52, 15);
+            this.labelSeireki.TabIndex = 0;
+            this.labelSeireki.Text = "西暦年";
+            this.labelSeireki.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxToshi
+            // 
+            this.textBoxToshi.Location = new System.Drawing.Point(15, 27);
+            this.textBoxToshi.Name = "textBoxToshi";
+            this.textBoxToshi.Size = new System.Drawing.Size(79, 22);
+            this.textBoxToshi.TabIndex = 1;
+            // 
+            // labelTsuki
+            // 
+            this.labelTsuki.AutoSize = true;
+            this.labelTsuki.Location = new System.Drawing.Point(120, 9);
+            this.labelTsuki.Name = "labelTsuki";
+            this.labelTsuki.Size = new System.Drawing.Size(22, 15);
+            this.labelTsuki.TabIndex = 2;
+            this.labelTsuki.Text = "月";
+            // 
+            // numericTsuki
+            // 
+            this.numericTsuki.Location = new System.Drawing.Point(123, 28);
+            this.numericTsuki.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericTsuki.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericTsuki.Name = "numericTsuki";
+            this.numericTsuki.Size = new System.Drawing.Size(58, 22);
+            this.numericTsuki.TabIndex = 3;
+            this.numericTsuki.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // labelHi
+            // 
+            this.labelHi.AutoSize = true;
+            this.labelHi.Location = new System.Drawing.Point(208, 9);
+            this.labelHi.Name = "labelHi";
+            this.labelHi.Size = new System.Drawing.Size(22, 15);
+            this.labelHi.TabIndex = 4;
+            this.labelHi.Text = "日";
+            // 
+            // numericHi
+            // 
+            this.numericHi.Location = new System.Drawing.Point(211, 28);
+            this.numericHi.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numericHi.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericHi.Name = "numericHi";
+            this.numericHi.Size = new System.Drawing.Size(58, 22);
+            this.numericHi.TabIndex = 5;
+            this.numericHi.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // labelYoubi
+            // 
+            this.labelYoubi.AutoSize = true;
+            this.labelYoubi.Location = new System.Drawing.Point(302, 30);
+            this.labelYoubi.Name = "labelYoubi";
+            this.labelYoubi.Size = new System.Drawing.Size(43, 15);
+            this.labelYoubi.TabIndex = 6;
+            this.labelYoubi.Text = "label1";
+            // 
+            // buttonSanshutsu
+            // 
+            this.buttonSanshutsu.Location = new System.Drawing.Point(12, 68);
+            this.buttonSanshutsu.Name = "buttonSanshutsu";
+            this.buttonSanshutsu.Size = new System.Drawing.Size(392, 34);
+            this.buttonSanshutsu.TabIndex = 7;
+            this.buttonSanshutsu.Text = "曜日算出";
+            this.buttonSanshutsu.UseVisualStyleBackColor = true;
+            this.buttonSanshutsu.Click += new System.EventHandler(this.buttonSanshutsu_Click);
+            // 
+            // FormYoubi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 114);
+            this.Controls.Add(this.buttonSanshutsu);
+            this.Controls.Add(this.labelYoubi);
+            this.Controls.Add(this.numericHi);
+            this.Controls.Add(this.labelHi);
+            this.Controls.Add(this.numericTsuki);
+            this.Controls.Add(this.labelTsuki);
+            this.Controls.Add(this.textBoxToshi);
+            this.Controls.Add(this.labelSeireki);
+            this.Name = "FormYoubi";
+            this.Text = "曜日算出";
+            this.Load += new System.EventHandler(this.FormYoubi_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericTsuki)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericHi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSeireki;
+        private System.Windows.Forms.TextBox textBoxToshi;
+        private System.Windows.Forms.Label labelTsuki;
+        private System.Windows.Forms.NumericUpDown numericTsuki;
+        private System.Windows.Forms.Label labelHi;
+        private System.Windows.Forms.NumericUpDown numericHi;
+        private System.Windows.Forms.Label labelYoubi;
+        private System.Windows.Forms.Button buttonSanshutsu;
+    }
+}
+
---
+++ WeekOfDay/WeekOfDay/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeekOfDay
+{
+    public partial class FormYoubi : Form
+    {
+        int tsuki = 1;
+        int hi = 1;
+        int toshi;
+        bool isUru;
+        bool isformat;
+        int youbi;
+        public FormYoubi()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormYoubi_Load(object sender, EventArgs e)
+        {
+            labelYoubi.Text = "";
+        }
+
+
+        private void buttonSanshutsu_Click(object sender, EventArgs e)
+        {
+            try{
+                Urudoshi();
+                FormatCheck();
+                hi = int.Parse(numericHi.Text);
+                YoubiSanshutsu();
+            }
+            catch (Exception ex)
+            {
+                labelYoubi.Text = "西暦年エラー";
+            }
+           
+        }
+
+
+        private bool Urudoshi()
+        {
+            toshi = int.Parse(textBoxToshi.Text);
+            isUru = false;
+            if ((toshi % 4 == 0 && toshi % 100 != 0) || toshi % 400 == 0)  // != ← NOT＝   || ← OR
+            {
+                isUru = true;
+            }
+
+            return isUru;
+        }
+        private bool FormatCheck()
+        {
+            isformat = true;
+            tsuki = int.Parse(numericTsuki.Text);
+            hi = int.Parse(numericHi.Text);
+            switch (tsuki)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    if ((isUru == true && hi > 28) || hi > 31)
+                    {
+                        isformat = false;
+                    }
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    if ((isUru == true && hi > 28) || hi > 30)
+                    {
+                        isformat = false;
+                    }
+                    break;
+                case 2:
+                    if (isUru == true && hi > 29)
+                    {
+                        isformat = false;
+                    }
+                    else if (isUru == false && hi > 28)
+                    {
+                        isformat = false;
+                    }
+                    break;
+
+            }
+            return isformat;
+        }
+        private void YoubiSanshutsu() //算出するところから
+        {
+            switch (tsuki)
+            {
+                case 1:
+                    toshi -= 1;
+                    youbi = (5 * toshi / 4 - toshi / 100 + toshi / 400 + (26 * 13 + 16) / 10 + hi) % 7;
+                    break;
+                case 2:
+                    toshi -= 1;
+                    youbi = (5 * toshi / 4 - toshi / 100 + toshi / 400 + (26 * 14 + 16) / 10 + hi) % 7;
+                    break;
+                default:
+                    youbi = (5 * toshi / 4 - toshi / 100 + toshi / 400 + (26 * tsuki + 16) / 10 + hi) % 7;
+                    break;
+            } 
+
+            if (isformat == true)
+            {
+
+                switch (youbi)
+                {
+                    case 0:
+                        labelYoubi.Text = "日曜日です";
+                        break;
+                    case 1:
+                        labelYoubi.Text = "月曜日です";
+                        break;
+                    case 2:
+                        labelYoubi.Text = "火曜日です";
+                        break;
+                    case 3:
+                        labelYoubi.Text = "水曜日です";
+                        break;
+                    case 4:
+                        labelYoubi.Text = "木曜日です";
+                        break;
+                    case 5:
+                        labelYoubi.Text = "金曜日です";
+                        break;
+                    case 6:
+                        labelYoubi.Text = "土曜日です";
+                        break;
+                }
+                
+                if (toshi < 0)
+                {
+                    labelYoubi.Text = "西暦年エラー";
+                }
+            }
+            else
+            {
+                labelYoubi.Text = "あり得ない日付";
+            }
+        }
+
+
+    }
+}
