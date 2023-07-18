8cd868b7c9b77cb63d92787f09d3f3f32c1fb558
村田聖希 <wiz.muramasa.111@gmail.com>
2023-07-18 14:49:14 +0900

    期末課題提出

---
+++ Question5/Question5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問5
+ * メソッドAddNumを定義して、メソッドMain内で呼び出しなさい。
+ * メソッド定義は下記の通りにすること
+ * 
+ * メソッド名：AddNum
+ * 戻り値の型：整数型
+ * 引数列　　：整数型 num1、整数型 num2
+ * 処理　　　：引数で渡されてきた2つの値を足し算した結果を戻り値として返す。
+ * 
+*/
+
+namespace Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 1, n2 = 2, sum;
+
+            sum = AddNum(n1, n2);
+
+            Console.WriteLine(sum);
+        }
+
+        public static int AddNum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Question6/Question6/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+            int change = int.Parse(textBoxChange.Text);
+
+            label5000yen.Text = (change / 5000).ToString();
+            change = change % 5000;
+
+            label1000yen.Text = (change / 1000).ToString();
+            change = change % 1000;
+
+            label500yen.Text = (change / 500).ToString();
+            change = change % 500;
+
+            label100yen.Text = (change / 100).ToString();
+            change = change % 100;
+
+            label50yen.Text = (change / 50).ToString();
+            change = change % 50;
+
+            label10yen.Text = (change / 10).ToString();
+            change = change % 10;
+
+            label5yen.Text = (change / 5).ToString();
+            change = change % 5;
+
+            label1yen.Text = (change / 1).ToString();
+        }
+    }
+}
