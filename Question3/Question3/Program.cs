21dbae150d4bf066d2cf0d148dd7a6b0667658d5
MSI\kisar <yamazakisouhei>
2023-07-18 16:04:33 +0900

    期末

---
+++ Question3/Question3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問3
+ * ２つの整数の入力（コンソールからの入力）を受け取り、
+ * 足し算した結果を出力（表示）するプログラムを書きなさい。
+ * 
+ * 入力例：
+ * 11
+ * 25
+ * 
+ * 出力例：
+ * 36
+ * 
+*/
+
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            string input = Console.ReadLine();
+            int inputNum = int(inputNum + inputNum);
+            Console.ReadLine();
+        }
+    }
+}
