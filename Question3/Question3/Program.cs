70acf7f19d8aec640df0c77498c2ea1df317c20a
ruiiiiiiiiiiiiiiiiiii <rui101304@gmail.com>
2023-07-18 16:12:56 +0900

    k

---
+++ Question3/Question3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問3
+ * ２つの整数の入力（コンソールからの入力）を受け取り、
+ * 足し算した結果を出力（表示）するプログラムを書きなさい。
+ * 
+ * 入力例：
+ * 11
+ * 25
+ * 
+ * 出力例：
+ * 36
+ * 
+*/
+
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          String  kazu;
+          String kazu2;
+          int goukei;
+            int a;
+            int b;
+            
+
+            kazu = Console.ReadLine();
+            kazu2 = Console.ReadLine();
+
+             a = int.Parse(kazu);
+             b = int.Parse(kazu2);
+
+
+            goukei = a + b;
+
+            Console.WriteLine(goukei);
+        }
+    }
+}
---
+++ Question4/Question4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問4
+ * 配列arrを定義し、配列内の値を全てfor文のループの中で出力（表示）しなさい。
+ * 配列arrは整数型の配列、要素数は『5』、添え字0～4にはそれぞれ
+ * 『10』『20』『30』『40』『50』
+ * の値を格納しなさい。
+*/
+
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arr;
+            int youso;
+            arr = 0;
+
+
+            for ( youso =0; youso<= 4; youso++) 
+            {
+                Console.WriteLine( youso + " ");
+
+            }
+        }
+    }
+}
