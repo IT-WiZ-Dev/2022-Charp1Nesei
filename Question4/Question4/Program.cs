b6e97f61c9674c7315d4bb62b244270082e2921c
mottiriOMOTI <kamameeee12212@gmail.com>
2023-07-18 15:51:20 +0900

    前期期末問題の追加

---
+++ Question4/Question4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問4
+ * 配列arrを定義し、配列内の値を全てfor文のループの中で出力（表示）しなさい。
+ * 配列arrは整数型の配列、要素数は『5』、添え字0～4にはそれぞれ
+ * 『10』『20』『30』『40』『50』
+ * の値を格納しなさい。
+*/
+
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //配列の宣言
+            int[] arr;
+            arr = new int[5];
+            int num; //添字
+            
+            
+            for(num = 0; num < 5; num++)
+            {
+
+                //代入のループ文
+                for(int i = 0; i <= num; i++ )
+                {
+                    
+                    arr[num] += 10;
+
+                }
+
+                //出力
+                Console.WriteLine(arr[num]);
+
+            }
+        }
+    }
+}
---
+++ Question6/Question6/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+            int val = 0;
+            CheckMoney(textBoxChange.Text,out val);
+
+            //もし入力された値が整数でなかったらエラーとして処理
+            if (val < 0)
+            {
+                textBoxChange.Text = "Error";
+            }
+            else
+            {
+                //本題のメソッド
+                Money(int.Parse(textBoxChange.Text));
+            }
+        }
+
+
+        //入力された値が整数かどうか判定するメソッド
+        private void CheckMoney(string num,out int val)
+        {
+            if (int.TryParse(num, out val) == false)
+              val = -1;
+        }
+
+
+
+        //ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+        //お釣りをラベルに代入するメソッド
+        //YOURLIFE：残りの金額
+        //Chargeがお釣りの枚数
+        private void Money(int YOURLIFE)
+        {
+            int Charge;
+
+            //5000円
+            Charge = YOURLIFE / 5000;
+            label5000yen.Text = "X" + Charge;
+            YOURLIFE %= 5000;
+
+            //1000円
+            Charge = YOURLIFE / 1000;
+            label1000yen.Text = "X" + Charge;
+            YOURLIFE %= 1000;
+
+            //500円
+            Charge = YOURLIFE / 500;
+            label500yen.Text = "X" + Charge;
+            YOURLIFE %= 500;
+
+            //100円
+            Charge = YOURLIFE / 100;
+            label100yen.Text = "X" + Charge;
+            YOURLIFE %= 100;
+
+            //50円
+            Charge = YOURLIFE / 50;
+            label50yen.Text = "X" + Charge;
+            YOURLIFE %= 50;
+
+            //10円
+            Charge = YOURLIFE / 10;
+            label10yen.Text = "X" + Charge;
+            YOURLIFE %= 10;
+
+            //5円
+            Charge = YOURLIFE / 5;
+            label5yen.Text = "X" + Charge;
+            YOURLIFE %= 5;
+
+            //1円
+            Charge = YOURLIFE;
+            label1yen.Text = "X" + Charge;
+        }
+
+    }
+}
