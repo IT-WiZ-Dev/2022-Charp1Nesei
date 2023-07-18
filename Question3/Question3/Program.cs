4843cb3a5a79ba8f025154036c0017b9f5c0f8fb
HAYATO-PC\hayato <hayato810honda@gmail.com>
2023-07-18 16:12:27 +0900

    k

---
+++ Question3/Question3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問3
+ * ２つの整数の入力（コンソールからの入力）を受け取り、
+ * 足し算した結果を出力（表示）するプログラムを書きなさい。
+ * 
+ * 入力例：
+ * 11
+ * 25
+ * 
+ * 出力例：
+ * 36
+ * 
+*/
+
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("ひとつめの数字");
+            String inputString = Console.ReadLine();
+
+            while (inputString != "quit")
+            {
+
+                a = Int32.Parse(inputString);
+
+                Console.WriteLine("ふたつめの数字");
+                inputString = Console.ReadLine();
+                b = Int32.Parse(inputString);
+
+                Console.WriteLine("合計" + (a + b) + "です");
+            }
+        }
+    }
+}
---
+++ Question4/Question4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問4
+ * 配列arrを定義し、配列内の値を全てfor文のループの中で出力（表示）しなさい。
+ * 配列arrは整数型の配列、要素数は『5』、添え字0～4にはそれぞれ
+ * 『10』『20』『30』『40』『50』
+ * の値を格納しなさい。
+*/
+
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numarr = new int[5];
+
+            numarr[0] = 10;
+            numarr[1] = 20;
+            numarr[2] = 30;
+            numarr[3] = 40;
+            numarr[4] = 50;
+
+            int sum2= numarr[0] + numarr[1] + numarr[2] + numarr[3] + numarr[4];
+        }
+    }
+}
