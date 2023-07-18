2c60b5c5554d65a2476b64cd6749906ec9420823
YOSIDA\yoshi <yoshdia1106.k@gmail.com>
2023-07-18 15:58:44 +0900

    前期期末課題追加

---
+++ splitCost/Question5/Question5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問5
+ * メソッドAddNumを定義して、メソッドMain内で呼び出しなさい。
+ * メソッド定義は下記の通りにすること
+ * 
+ * メソッド名：AddNum
+ * 戻り値の型：整数型
+ * 引数列　　：整数型 num1、整数型 num2
+ * 処理　　　：引数で渡されてきた2つの値を足し算した結果を戻り値として返す。
+ * 
+*/
+
+namespace Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        private static int AddNum(int num1,int num2)
+        {
+           
+            
+            int num3 = num1 + num2;
+
+            return num3;
+        }
+        
+    }
+}
