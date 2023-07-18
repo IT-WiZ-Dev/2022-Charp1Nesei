9267e389caededcef6514544267f42975bdc625b
MSI\kaede <kaede.0322.lobin@gmail.com>
2023-07-18 15:27:40 +0900

    前期期末課題提出

---
+++ Question4/Question4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問4
+ * 配列arrを定義し、配列内の値を全てfor文のループの中で出力（表示）しなさい。
+ * 配列arrは整数型の配列、要素数は『5』、添え字0～4にはそれぞれ
+ * 『10』『20』『30』『40』『50』
+ * の値を格納しなさい。
+*/
+
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int j = 10;
+            for(int i=0;i<5;i++)
+            {
+                arr[i] = j;
+                Console.WriteLine(arr[i]);
+                j += 10;
+            }
+        }
+    }
+}
---
+++ Question6/Question6/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxChange_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+            int oturi,gos,se,goh,hya,goj,ju,go,iti;
+            oturi = int.Parse(textBoxChange .Text);
+            gos = oturi / 5000;
+            oturi = oturi % 5000;
+            se = oturi / 1000;
+            oturi = oturi % 1000;
+            goh = oturi / 500;
+            oturi = oturi % 500;
+            hya = oturi / 100;
+            oturi = oturi % 100;
+            goj = oturi / 50;
+            oturi = oturi % 50;
+            ju = oturi / 10;
+            oturi = oturi % 10;
+            go = oturi / 5;
+            oturi = oturi % 5;
+            iti = oturi;
+            label5000yen.Text = gos.ToString ();
+            label1000yen.Text = se.ToString();
+            label500yen.Text = goh.ToString();
+            label100yen.Text = hya.ToString();
+            label50yen.Text = goj.ToString();
+            label10yen.Text = ju.ToString();
+            label5yen.Text = go.ToString();
+            label1yen.Text = iti.ToString();
+
+        }
+    }
+}
