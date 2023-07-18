f0f5c2636ca23cf758406e4dba6b270ffe5ccd7b
MSI\igada <daiki20050226@gmail.com>
2023-07-18 15:01:27 +0900

    前期期末試験　解答

---
+++ Question1/Question1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 問１
+ * 
+ * 次のMain関数内で宣言されている変数のデータ型を適切なものに修正して下さい。 
+ *
+ * 
+*/
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value1 = "10";
+            bool value2 = true;
+            char value3 = 'r';
+            double value4 = 20.0;
+            int value5 = 10;
+        }
+    }
+}
---
+++ Question6/Question6/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCul_Click(object sender, EventArgs e)
+        {
+            int change = int.Parse(textBoxChange.Text);
+
+            int yen = 0;
+            while(change>=5000)
+            {
+                change-=5000;
+                yen++;
+            }
+            label5000yen.Text = yen.ToString();
+            yen = 0;
+
+            while (change >= 1000)
+            {
+                change -= 1000;
+                yen++;
+            }
+            label1000yen.Text = yen.ToString();
+            yen = 0;
+
+            while (change >= 500)
+            {
+                change -= 500;
+                yen++;
+            }
+            label500yen.Text = yen.ToString();
+            yen = 0;
+
+            while (change >= 100)
+            {
+                change -= 100;
+                yen++;
+            }
+            label100yen.Text = yen.ToString();
+            yen = 0;
+
+            while (change >= 50)
+            {
+                change -= 50;
+                yen++;
+            }
+            label50yen.Text = yen.ToString();
+            yen = 0;
+
+            while (change >= 10)
+            {
+                change -= 10;
+                yen++;
+            }
+            label10yen.Text = yen.ToString();
+            yen = 0;
+
+            while (change >= 5)
+            {
+                change -= 5;
+                yen++;
+            }
+            label5yen.Text = yen.ToString();
+
+            label1yen.Text = change.ToString();
+        }
+    }
+}
