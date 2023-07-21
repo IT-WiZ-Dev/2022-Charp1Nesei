893e0ad1caf96f4e4249285e8e99873be3776ac9
shiyon4410 <siyon6954@au.com>
2023-07-21 14:50:56 +0900

    第四練習問題を追加

---
+++ 曜日算出/曜日算出/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace weekofday
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.year1 = new System.Windows.Forms.Label();
+            this.month1 = new System.Windows.Forms.Label();
+            this.day1 = new System.Windows.Forms.Label();
+            this.week = new System.Windows.Forms.Label();
+            this.year = new System.Windows.Forms.TextBox();
+            this.go = new System.Windows.Forms.Button();
+            this.month = new System.Windows.Forms.NumericUpDown();
+            this.day = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.month)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.day)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // year1
+            // 
+            this.year1.AutoSize = true;
+            this.year1.Location = new System.Drawing.Point(73, 35);
+            this.year1.Name = "year1";
+            this.year1.Size = new System.Drawing.Size(52, 15);
+            this.year1.TabIndex = 0;
+            this.year1.Text = "西暦年";
+            // 
+            // month1
+            // 
+            this.month1.AutoSize = true;
+            this.month1.Location = new System.Drawing.Point(224, 36);
+            this.month1.Name = "month1";
+            this.month1.Size = new System.Drawing.Size(22, 15);
+            this.month1.TabIndex = 1;
+            this.month1.Text = "月";
+            // 
+            // day1
+            // 
+            this.day1.AutoSize = true;
+            this.day1.Location = new System.Drawing.Point(350, 35);
+            this.day1.Name = "day1";
+            this.day1.Size = new System.Drawing.Size(22, 15);
+            this.day1.TabIndex = 2;
+            this.day1.Text = "日";
+            // 
+            // week
+            // 
+            this.week.AutoSize = true;
+            this.week.Location = new System.Drawing.Point(479, 56);
+            this.week.Name = "week";
+            this.week.Size = new System.Drawing.Size(88, 15);
+            this.week.TabIndex = 3;
+            this.week.Text = "曜日が出ます";
+            // 
+            // year
+            // 
+            this.year.Location = new System.Drawing.Point(76, 53);
+            this.year.Name = "year";
+            this.year.Size = new System.Drawing.Size(100, 22);
+            this.year.TabIndex = 5;
+            this.year.TextChanged += new System.EventHandler(this.year_TextChanged);
+            // 
+            // go
+            // 
+            this.go.Location = new System.Drawing.Point(76, 128);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(446, 23);
+            this.go.TabIndex = 6;
+            this.go.Text = "曜日算出";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // month
+            // 
+            this.month.Location = new System.Drawing.Point(227, 54);
+            this.month.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.month.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.month.Name = "month";
+            this.month.Size = new System.Drawing.Size(81, 22);
+            this.month.TabIndex = 7;
+            this.month.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // day
+            // 
+            this.day.Location = new System.Drawing.Point(353, 53);
+            this.day.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.day.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.day.Name = "day";
+            this.day.Size = new System.Drawing.Size(83, 22);
+            this.day.TabIndex = 8;
+            this.day.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.day);
+            this.Controls.Add(this.month);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.year);
+            this.Controls.Add(this.week);
+            this.Controls.Add(this.day1);
+            this.Controls.Add(this.month1);
+            this.Controls.Add(this.year1);
+            this.Name = "Form1";
+            this.Text = "曜日算出";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.month)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.day)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label year1;
+        private System.Windows.Forms.Label month1;
+        private System.Windows.Forms.Label day1;
+        private System.Windows.Forms.Label week;
+        private System.Windows.Forms.TextBox year;
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.NumericUpDown month;
+        private System.Windows.Forms.NumericUpDown day;
+    }
+}
+
---
+++ 曜日算出/曜日算出/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace weekofday
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            week.Text = "";
+        }
+
+        private void year_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool Uruuyear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        private bool Errerday(int day, int month, bool Uruu)
+        {
+
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            {
+                return true;
+            }
+            else if (month == 4 || month == 6 || month == 9 || month == 11)
+            {
+                if (day <= 30)
+                {
+                    return true;
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            else 
+            {
+                if (day <= 28 || (day == 29 && Uruu == true))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+
+
+
+        }
+
+        private void YoubiKeisan(int day, int month, int year)
+        {
+            int akimaru;
+
+            if (month == 1 || month == 2)
+            {
+                month = month + 12;
+                year = year - 1;
+            }
+
+
+            akimaru = (5 * year / 4 - year / 100 + year / 400 + (26 * month + 16) / 10 + day) % 7;
+
+            switch(akimaru)
+            {
+                case 0:
+                    week.Text = "日曜日";
+                    break;
+                case 1:
+                    week.Text = "月曜日";
+                    break;
+                case 2:
+                    week.Text = "火曜日";
+                    break;
+                case 3:
+                    week.Text = "水曜日";
+                    break;
+                case 4:
+                    week.Text = "木曜日";
+                    break;
+                case 5:
+                    week.Text = "金曜日";
+                    break;
+                case 6:
+                    week.Text = "土曜日";
+                    break;
+                default:
+                    week.Text = "西暦年エラー";
+                    break;
+            }
+        }
+
+        private void go_Click(object sender, EventArgs e)
+        {
+            int Tosi;
+            if(int.TryParse(year.Text, out Tosi) == false)
+            {
+                week.Text = "西暦年エラー";
+            }
+            if(Tosi < 0)
+            {
+                week.Text = "西暦年エラー";
+            }
+
+            int hi = (int)day.Value;
+            int tuki = (int)month.Value;
+
+            bool uruu = Uruuyear(Tosi);
+            bool asahi = Errerday(hi, tuki, uruu);
+            if( asahi == false )
+            {
+                week.Text = "ありえない日付";
+            }
+
+            YoubiKeisan(hi, tuki, Tosi);
+
+
+        }
+    }
+}
