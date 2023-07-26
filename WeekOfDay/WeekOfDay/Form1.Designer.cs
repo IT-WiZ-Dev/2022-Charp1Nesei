e8e7c2ef945604bbd485a89c6644e2d9d1e68b0a
AzureAD\渡辺千紘(zeal23310032) <gm27360123@gmail.com>
2023-07-26 10:14:07 +0900

    章末問題4を提出

---
+++ WeekOfDay/WeekOfDay/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace WeekOfDay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxnen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownTuk = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownday = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelsan = new System.Windows.Forms.Label();
+            this.buttonkei = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTuk)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownday)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "西暦年";
+            // 
+            // textBoxnen
+            // 
+            this.textBoxnen.Location = new System.Drawing.Point(35, 60);
+            this.textBoxnen.Name = "textBoxnen";
+            this.textBoxnen.Size = new System.Drawing.Size(100, 25);
+            this.textBoxnen.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "月";
+            // 
+            // numericUpDownTuk
+            // 
+            this.numericUpDownTuk.Location = new System.Drawing.Point(158, 60);
+            this.numericUpDownTuk.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDownTuk.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownTuk.Name = "numericUpDownTuk";
+            this.numericUpDownTuk.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDownTuk.TabIndex = 3;
+            this.numericUpDownTuk.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownday
+            // 
+            this.numericUpDownday.Location = new System.Drawing.Point(321, 61);
+            this.numericUpDownday.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numericUpDownday.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownday.Name = "numericUpDownday";
+            this.numericUpDownday.Size = new System.Drawing.Size(99, 25);
+            this.numericUpDownday.TabIndex = 4;
+            this.numericUpDownday.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "日";
+            // 
+            // labelsan
+            // 
+            this.labelsan.AutoSize = true;
+            this.labelsan.Location = new System.Drawing.Point(469, 63);
+            this.labelsan.Name = "labelsan";
+            this.labelsan.Size = new System.Drawing.Size(52, 18);
+            this.labelsan.TabIndex = 6;
+            this.labelsan.Text = "label4";
+            // 
+            // buttonkei
+            // 
+            this.buttonkei.Location = new System.Drawing.Point(35, 109);
+            this.buttonkei.Name = "buttonkei";
+            this.buttonkei.Size = new System.Drawing.Size(486, 34);
+            this.buttonkei.TabIndex = 7;
+            this.buttonkei.Text = "曜日算出";
+            this.buttonkei.UseVisualStyleBackColor = true;
+            this.buttonkei.Click += new System.EventHandler(this.buttonkei_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 167);
+            this.Controls.Add(this.buttonkei);
+            this.Controls.Add(this.labelsan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDownday);
+            this.Controls.Add(this.numericUpDownTuk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxnen);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "曜日算出";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTuk)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownday)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxnen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownTuk;
+        private System.Windows.Forms.NumericUpDown numericUpDownday;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelsan;
+        private System.Windows.Forms.Button buttonkei;
+    }
+}
+
---
+++ WeekOfDay/WeekOfDay/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeekOfDay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelsan.Text = "";
+        }
+
+        //うるう年判定
+        private bool Urujudge()
+        {
+            int year = int.Parse(textBoxnen.Text);
+            bool isUru = false;
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                isUru = true;
+            }
+            return isUru;
+        }
+
+        //西暦年チェック
+        private void TextToValue(string text, out int val)
+        {
+            if (int.TryParse(text, out val) == false)
+                val = -1;
+        }
+
+        //日付があっているか判定
+        private bool Checkdate(bool isUru)
+        {
+            int month = (int)numericUpDownTuk.Value;
+            int day = (int)numericUpDownday.Value;
+            isUru = Urujudge();
+            bool isClear = true;
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    if (day > 31)
+                    {
+                        isClear = false;
+                    }
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    if (day > 30)
+                    {
+                        isClear = false;
+                    }
+                    break;
+                case 2:
+                    if (isUru)
+                    {
+                        if (day > 29)
+                        {
+                            isClear = false;
+                        }
+                    }
+                    else if (day > 28)
+                    {
+                        isClear = false;
+                    }
+                    break;
+            }
+            return isClear;
+        }
+
+        //牡丹栗kkkkkkkkっく
+        private void buttonkei_Click(object sender, EventArgs e)
+        {
+            string youbi = "";
+            TextToValue(textBoxnen.Text,out int val);
+            if (val < 0)
+            {
+                youbi = "西暦年エラー";
+            }
+            else
+            {
+                bool isUru = Urujudge();
+                bool isCheckdate = Checkdate(isUru);
+                int nen = int.Parse(textBoxnen.Text);
+                int month = (int)numericUpDownTuk.Value;
+                int day = (int)numericUpDownday.Value;
+                if (isCheckdate == false)
+                {
+                    youbi = "ありえない日付";
+                }
+                else
+                {
+
+                    switch (month)
+                    {
+                        case 1:
+                        case 2:
+                            nen = nen - 1;
+                            month = month + 12;
+                            break;
+                    }
+                    int w = (5 * nen / 4 - nen / 100 + nen /
+                        400 + (26 * month + 16) / 10 + day) % 7;
+                    switch (w)
+                    {
+                        case 0:
+                            youbi = "日曜日です";
+                            break;
+                        case 1:
+                            youbi = "月曜日です";
+                            break;
+                        case 2:
+                            youbi = "火曜日です";
+                            break;
+                        case 3:
+                            youbi = "水曜日です";
+                            break;
+                        case 4:
+                            youbi = "木曜日です";
+                            break;
+                        case 5:
+                            youbi = "金曜日です";
+                            break;
+                        case 6:
+                            youbi = "土曜日です";
+                            break;
+                    }
+                }
+            }
+            labelsan.Text = youbi;
+
+
+
+
+        }
+    }
+}
