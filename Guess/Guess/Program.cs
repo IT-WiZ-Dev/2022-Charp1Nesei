1de5618fd5a924571b8c8cbfd9a309b1602380ed
MSI\kaede <kaede.0322.lobin@gmail.com>
2023-11-08 09:51:46 +0900

    第五章練習問題追加

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int num = 32;
+            int count=1, max = 100,min=0;
+            Console.Write(min+"から"+max+"の間の数値を当ててください。>");
+            int input = int.Parse(Console.ReadLine());
+            while (input != num) {
+
+                if (input > max || input < min)
+                {
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                    Console.Write(min + "から" + max + "の間の数値を当ててください。>");
+                    input = int.Parse(Console.ReadLine());
+                }
+                if (input > num)
+                {
+
+                    count++;
+                    Console.WriteLine("答はもっと小さいです。");
+                    Console.Write("\n");
+                    max = input;
+                    Console.Write(min + "から" + (max-1) + "の間の数値を当ててください。>");
+                    max--;
+                    input = int.Parse(Console.ReadLine());
+
+                }
+                else
+                {
+
+                    count++;
+                    Console.WriteLine("答はもっと大きいです。");
+                    Console.Write("\n");
+                    min = input;
+                    Console.Write((min+1) + "から" + max + "の間の数値を当ててください。>");
+                    input = int.Parse(Console.ReadLine());
+                    min++;
+
+                }
+            }
+            if (input == num)
+            {
+                Console.WriteLine("おめでとう。" + count + "回目で当たりました。");
+            }
+        }
+    }
+}
---
+++ kuku/kuku/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  * * * 九九の表 * * *  ");
+            Console.Write("  |");
+            for (int i = 1; i <= 9;i++)
+            {
+                Console.Write("  " + i);
+            }
+            Console.Write("\n");
+            Console.WriteLine("-------------------------------");
+            int seki = 0;
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write(i + " |");
+                for (int j = 1; j <= 9; j++)
+                {
+                    seki = i * j;
+                    Console.Write("{0,3}",seki);                                                                                                                                                                                           
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
