433dc215fc8146433af5b81b4454b304d698bdc4
MSI\tirot <tuginohitirori@gmail.com>
2023-11-08 09:50:04 +0900

    第５章練習問題(九九の表)を追加

---
+++ Multiplication Table/Multiplication Table/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("　＊ ＊ ＊ 九九の表 ＊ ＊ ＊  ");
+            Console.Write("  |");
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write("  " + (i + 1));
+            }
+
+            Console.WriteLine();   //改行
+            Console.WriteLine("-------------------------------");
+
+            //九九を表示
+            for(int i = 0; i < 9; i++)
+            {
+                //(i + 1)の段を表示
+                Console.Write((i + 1) + " |");
+                for (int j = 0; j < 9; j++)
+                {
+                    //i + 1の段を表示する
+                    Console.Write("{0,3}",(i + 1) * (j + 1));
+                }
+                //改行
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}
