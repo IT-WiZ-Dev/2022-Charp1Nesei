44328f1e6d6a2ca08948faa572af1b34d2e0e99a
MSI\14092 <hikkyhachiman0920@gmail.com>
2023-11-08 09:52:14 +0900

    第5章　練習問題

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //当たりの数
+            const int num = 55;
+            int cnt = 0;
+            int high = 100, low = 0;
+            
+            //無限ループ
+            while (true)
+            {
+              
+                //入力カウントを１増やす
+                
+               
+                Console.Write(low + "から" + high + "の間の数値を当ててください。>");
+                int n = int.Parse(Console.ReadLine());
+                if (n > high || n < low)//範囲外の値を入力したときの処理
+                {
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                    
+                }
+                else if(n < num)//小さい値を入力したときの処理
+                {
+                    cnt++;
+                    Console.WriteLine("答えはもっと大きいです");
+                    low = n + 1;
+                }
+                else if(n > num)//大きい値を入力した時の処理
+                {
+                    cnt++;
+                    Console.WriteLine("答えはもっと小さいです");
+                    high = n - 1;
+                }
+                else //正しい値を入力したときの処理
+                {
+                    cnt++;
+                    Console.WriteLine("おめでとう。" + cnt + "回目で当たりました。");
+                    break;
+                }
+                
+            }
+        }
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   * * * 九九の表 * * *   ");
+            Console.Write("   |");
+            for(int i = 0; i < 9; i++)
+            {
+                Console.Write("  " + (i + 1));
+            }
+
+           
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------");
+
+            //九九の表示
+
+            for(int i = 1;i <= 9; i++)
+            {
+                Console.Write(i + "  |");
+                //(i+1)の段の表示　例
+                for(int j = 1; j <= 9; j++)
+                {
+                    Console.Write("{0,3}"," "+ (i*j));
+
+                }
+                //改行
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
