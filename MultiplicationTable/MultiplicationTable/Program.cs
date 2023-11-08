6e02b1687352b9a802c81e0a007d269698e45948
AzureAD\渡辺千紘(zeal23310032) <gm27360123@gmail.com>
2023-11-08 09:55:06 +0900

    練習問題5・例題５　追加しました

---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" * * * 九九の表 * * *");
+            Console.Write("   |");
+            for(int i = 0; i<9; i++)
+            {
+                Console.Write("  " + (i + 1));
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------------------------------------");
+            int val;
+            for(int i = 1;i<=9;i++)
+            {
+                Console.Write(i);
+                Console.Write("  |");
+                
+                for (int j = 1; j <= 9; j++)
+                {
+                    val = i * j;
+                    System.Console.Write("{0,3}", val.ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+            
+            Console.WriteLine("終了するには何かキーを押してください。");
+        }
+    }
+}
