d1b37bd39b5b19f60e63d05f1c34ab19985b4e1d
MSI\鈴木一弥 <ssk3kzy3.pe@icloud.com>
2023-11-08 09:54:59 +0900

    第５章練習問題

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //あたりの数
+            const int num = 55;
+            int count = 0;
+            int max = 100;
+            int min = 0;
+            
+
+            //無限ループ
+            while (true)
+            {
+                Console.WriteLine($"{min}から{max}の間の数値を当ててください。");
+                //入力を受けとる
+                int input = int.Parse(Console.ReadLine());
+                //入力カウントを１増やす
+                count++;
+                //範囲外の数値が入力されたとき
+                if (input < min || input > max)
+                {
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                    continue;
+                }
+
+                //正解の時の処理
+                if (input == num)
+                {
+                    Console.WriteLine("おめでとう。"+ (count) + "回目で当たりました。");
+                    break;
+                } 
+                 
+                //大きい数を入力した時の処理
+                if (input > num)
+                {
+                    Console.WriteLine("答えはもっと小さいです。");
+                    max = input - 1;
+                }
+
+                //小さい数を入力したときの処理
+                if (input < num)
+                {
+                    Console.WriteLine("答えはもっと大きいです。");
+                    min = input + 1; 
+                }
+
+            }
+        }
+    }
+}
