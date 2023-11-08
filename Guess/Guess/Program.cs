7a96f6a32a4245d536aba293def2b2e6ea299d4b
MSI\igada <daiki20050226@gmail.com>
2023-11-08 09:46:24 +0900

    5章練習問題提出

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int seikai = 50;
+            int min = 0;
+            int max = 100;
+            int tou = 0;
+            int kaisu = 1;
+            while (seikai!=tou)
+            {
+                Console.Write(min + "から" + max + "の間の数値を当ててください。> ");
+                tou=int.Parse(Console.ReadLine());
+
+                if (tou>max || tou<min)
+                {
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                }
+                else if (tou>seikai)
+                {
+                    Console.WriteLine("答はもっと小さいです。");
+                    max = tou - 1;
+                    kaisu++;
+                }
+                else if (tou<seikai)
+                {
+                    Console.WriteLine("答はもっと大きいです。");
+                    min = tou + 1;
+                    kaisu++;
+                }
+            }
+            Console.WriteLine("おめでとう。" + kaisu + "回目であたりました。");
+
+
+        }
+    }
+}
---
+++ Multiplication Table/Multiplication Table/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   * * * 九九の表 * * *");
+            Console.WriteLine("   |  1  2  3  4  5  6  7  8  9");
+            Console.WriteLine("----------------------------------");
+
+            for (int i=1; i<10; i++)
+            {
+                Console.Write(i+"  |");
+                for(int j=1; j<10; j++)
+                {
+                    Console.Write($"{j*i,3}");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
