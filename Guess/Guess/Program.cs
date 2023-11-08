09669919079419937a5df8625f0fb54d086339db
DESKTOP-EICL5GD\hsout <hasimoto.s511@gmail.com>
2023-11-08 09:50:22 +0900

    P177と章末課題提出

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 0;
+            int max = 100;
+            int kai=0;
+            Random num = new Random();
+            int atr = num.Next(0, 100);
+            Console.WriteLine(atr);
+
+            for (int i=1;i<100;i++)
+            {
+                Console.Write(min + "から" + max + "の間の数値を当ててください。＞");
+                int n = int.Parse(Console.ReadLine());
+               
+                if (n == atr)
+                {
+                    kai = i;
+                    Console.WriteLine("おめでとう。" + kai + "回目で当たりました。");
+                    break;
+                }
+                else if(n<atr)    //  入力が当たり以下
+                {
+                    Console.WriteLine("答はもっと大きいです。");
+                    min = n + 1;
+                }
+                else if(n>atr)
+                {
+                    Console.WriteLine("答はもっと小さいです。");
+                    max = n - 1;
+                }
+            }
+           
+
+        }
+    }
+}
---
+++ StudyP177/StudyP177/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyP177
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   ＊＊＊九九の表＊＊＊ ");
+            Console.Write("   |");
+
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write("  "+i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------");
+
+            for(int i=1;i<=9;i++)
+            {
+                Console.Write(i+"  "+"|");
+
+                for (int j=1;j<=9;j++)
+                {
+                    Console.Write("{0,3}",i*j);
+
+                }
+                Console.WriteLine();
+
+
+            }
+
+
+
+        }
+    }
+}
