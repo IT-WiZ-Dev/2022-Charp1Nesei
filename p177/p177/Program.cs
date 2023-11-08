c11c8f20eb23f3d97a4cbdd2bb5d94e9f4d8a05d
shiyon4410 <siyon6954@au.com>
2023-11-08 09:49:25 +0900

    第五章練習問題を追加

---
+++ p177/p177/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p177
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   ＊＊＊　九九の表　＊＊＊   ");
+            Console.Write("   |");
+            for (int i = 0; i < 9; i++) 
+            {
+                Console.Write( "  " + ( i+ 1) );
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            //九九を表示
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write(i + "  |");
+                
+                for (int j = 1; j <= 9; j ++)
+                {
+                    Console.Write("{0,3}",i*j);
+                    
+
+
+
+
+
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ p178/p178/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p178
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //あたりの数の宣言
+            Random r = new Random();
+            int num = r.Next(1,100);
+            int count = 0;
+            int max = 100;
+            int min = 0;
+
+                //無限ループ
+                while (true)
+            {
+
+                Console.Write(min +"から" + max + "の間の数値を当ててください>");
+                //入力受け取り
+                int input = int.Parse(Console.ReadLine());
+                //入力されるごとに入力カウントを1ずつ増やす
+                count++;
+
+                // 正解の時
+                if (input == num)
+                {        
+                    Console.WriteLine("おめでとう。" + count + "回目であたりました");
+                    break;
+                }
+                //例外処理
+                if (input > max || input < min)
+                {
+                    Console.WriteLine("入力値が正しくありません、再入力してください");
+                    count--;
+                }
+                //数が小さいとき
+                else if (input < num)
+                {
+                    Console.WriteLine("答はもっと大きいです");
+                    min = input + 1;
+                }
+                //数が大きいとき
+                else if (input > num)
+                {
+                    Console.WriteLine("答はもっと小さいです");
+                    max = input - 1;
+                }
+
+                else
+                {
+                    Console.WriteLine("入力値が正しくありません、再入力してください");
+                    count--;
+                }
+
+
+
+
+
+
+
+            }
+
+
+
+        }
+    }
+}
