eee0f7d0af842067f6eb492ac935d3b9ffad5b27
mottiriOMOTI <kamameeee12212@gmail.com>
2023-11-08 09:50:02 +0900

    数当てゲームと九九表の表示のプログラム (177p,178p)の追加

---
+++ Kuku/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  ＊＊＊九九の表＊＊＊  ");
+            Console.Write("   |");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write(" " + i +"|" );
+            }
+            Console.WriteLine();
+            Console.WriteLine("_________________________________");
+
+            //九九表示
+            for (int i = 0; i < 9;i++)
+            {
+                //( i + 1 )の段の表示 例「1  |」
+                Console.Write( ( i + 1 ) + " |");
+
+                for (int j = 0; j < 9; j++)
+                {
+                    //( j + 1 )の段表示
+                    int K = ( i + 1 ) * ( j + 1 );
+                    Console.Write("{0,3}",K );
+                }
+                //改行
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ kazuate/ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 当たりの数
+            const int num = 55;
+            int count = 0;
+            int High = 100, Low = 0;
+            //無限ループ
+            while (true)
+            {
+                
+                Console.WriteLine(Low + "から" + High + "の間の数値を当ててください。");
+                int input = int.Parse(Console.ReadLine());
+
+                if (input < Low || input > High)
+                {
+                    Console.WriteLine("入力値が正しくありません。やり直せィ！");
+                    continue;
+                }
+                count++;
+
+                //正解のとき
+                if (num == input)
+                {
+                    Console.WriteLine("おめでとう。" + count + "回目で当たりました。");
+                    break;
+                }
+                else if (num < input) //不正解のとき(でかい)
+                {
+                    Console.WriteLine("答はもっと小さいです。");
+                    High = input - 1;
+                }
+                else if (num > input) //不正解のとき(ちいさい)
+                {
+                    Console.WriteLine("答はもっと大きいです。");
+                    Low = input + 1;
+                }
+                 
+            }
+        }
+    }
+}
