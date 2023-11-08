68da1eca019aca60699c78aa6f1396a09bf62367
MSI\tirot <tuginohitirori@gmail.com>
2023-11-08 09:52:24 +0900

    第５章練習問題(数当てゲーム)を追加

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //当たりの数
+            const int num = 55;     //constを付けることによって定数化
+            int count = 0;
+            int min = 0;
+            int max = 100;
+            Console.Write(min + "から" + max + "の間の数値を当ててください。＞");
+
+            while (true)
+            {
+                int input = int.Parse(Console.ReadLine());
+                count++;
+                if(input < min || input > max)
+                {
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                    Console.Write(min + "から" + max + "の間の数値を当ててください。＞");
+                    continue;
+                }
+
+                if (input > num)
+                {
+                    max = input - 1;
+                    Console.WriteLine("答えはもっと小さいです。");
+                    Console.WriteLine();
+                    Console.Write(min + "から" + max + "の間の数値を当ててください。＞");
+                }
+
+                if(input < num)
+                {
+                    min = input + 1;
+                    Console.WriteLine("答えはもっと大きいです。");
+                    Console.WriteLine();
+                    Console.Write(min + "から" + max + "の間の数値を当ててください。＞");
+                }
+
+                if(input == num)
+                {
+                    Console.WriteLine("おめでとう。" + count + "回目で当たりました。");
+                    Console.WriteLine();
+                    break;
+                }
+            }
+        }
+    }
+}
