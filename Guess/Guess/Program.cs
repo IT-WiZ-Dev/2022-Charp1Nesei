3a538693f2324cb5ec43e049bb226b2a31b9416a
宮下　吟侍 <adgjmptw565@ickoud.com>
2023-11-08 09:51:29 +0900

    章末課題５追加しました。

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int targetNumber =55;
+            int attempts = 0;
+
+            Console.WriteLine("0から100の間の数値を当ててください。");
+
+            int l = 0;
+            int h = 100;
+
+            while (true)
+            {
+          
+                string input = Console.ReadLine();
+                int userNumber;
+
+                if (int.TryParse(input, out userNumber))
+                {
+                    if (userNumber < l || userNumber > h)
+                    {
+                        Console.WriteLine("入力値が正しくありません。");
+                        Console.WriteLine(l + "から" + h + "の間の数値を当ててください。");
+                        continue;
+                    }
+
+                    attempts++;
+
+                    if (userNumber < targetNumber)
+                    {
+                        Console.WriteLine("もっと大きな数です。");
+                        l = userNumber;
+                        Console.WriteLine(l + "から" + h + "の間の数値を当ててください。");
+                    }
+                    else if (userNumber > targetNumber)
+                    {
+                        Console.WriteLine("もっと小さな数です。");
+                        h = userNumber;
+                        Console.WriteLine(l + "から" + h + "の間の数値を当ててください。");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"おめでとう。{attempts}回目で当たりました！");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                    Console.WriteLine(l + "から" + h + "の間の数値を当ててください。");
+
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ MUltiplicationTable/MUltiplicationTable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUltiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("　　＊＊＊九九の表＊＊＊");
+            Console.WriteLine(" |　1   2   3   4   5   6   7   8   9");
+            Console.WriteLine("----------------------------------------");
+
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write(i + "|");
+
+                for (int j = 1; j <= 9; j++)
+                {
+                    int result = i * j;
+                    Console.Write($"{result,3}"); // 3桁分の幅で表示
+
+                    if (j < 9)
+                    {
+                        Console.Write(" "); // マスを区切る空白を追加
+                    }
+                }
+
+                Console.WriteLine(); // 次の行に移行
+            }
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.ReadLine(); // プログラムを一時停止するための入力待ちを追加
+        }
+    }
+}
