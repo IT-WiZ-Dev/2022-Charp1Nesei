a61119128837fb789e9f100fa1cdc7c1d866f62e
akimaru kimura <mingwanmucun720@i.softbank.jp>
2023-11-08 09:48:01 +0900

    第五章練習問題追加

---
+++ Guess/Guess/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 100, min = 0, num,count = 0,n;
+            num = 55;
+
+            while (true)
+            {
+                Console.Write(min+"から"+max+"の間の数値を当ててください。　＞　");
+                n = int.Parse(Console.ReadLine());
+
+                if (n > max || n < min)
+                {                   
+                    Console.WriteLine("入力値が正しくありません。再入力してください。");
+                }
+
+                else if (num < n)
+                {
+                    count++;
+                    Console.WriteLine("答えはもっと小さいです");
+                    max = n - 1;
+                }
+
+                else if(num > n)
+                {
+                    count++;
+                    Console.WriteLine("答えはもっと大きいです");
+                    min = n + 1;
+                }
+
+                else
+                {
+                    count++;
+                    Console.WriteLine("おめでとう。"+count+"回目で当たりました。");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ans;
+            Console.WriteLine("* * * 九九の表 * * *");
+            Console.Write("   ");
+            for (int n = 1;n <= 9;n++)
+            {
+                Console.Write(" "+$"{n,3}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("ーーーーーーーーーーーーーーーーーーーーー");
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    ans = i * j;
+                    if (j == 1) 
+                    {
+                        Console.Write(i + " | " + $"{ans,3}");
+                    }
+                    else
+                    {
+                        Console.Write(" "+$"{ans,3}");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
